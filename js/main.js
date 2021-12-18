5e24884ec2e22a1d0f9f62fa82f23c1780405279
zotovalexrew <che123zotov@gmail.com>
2021-12-18 17:01:27 +0300

    minifix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,69 @@
+  $(document).ready(function(){
+var burger = document.querySelector(".burger");
+burger.addEventListener("click", function () {
+  // console.log("клик по кнопке меню");
+  document.querySelector(".navbar-right").classList.toggle("navbar-right--visible");
+});
+
+// модальное окно
+var modalButton = $("[data-toggle=modal]");
+var closeModalButton = $(".modal__close");
+modalButton.on("click", openModal);
+closeModalButton.on("click", closeModal);
+
+var modalButton = $("[data-toggle=modal]");
+var closeModalButton = $(".modal__overlay");
+modalButton.on("click", openModal);
+closeModalButton.on("click", closeModal);
+
+function openModal() {
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.addClass("modal__overlay--visible")
+  modalDialog.addClass("modal__dialog--visible")
+  $("body").addClass("modal-open");
+}
+function closeModal(event) {
+  event.preventDefault()
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.removeClass("modal__overlay--visible")
+  modalDialog.removeClass("modal__dialog--visible")
+  $("body").removeClass("modal-open");
+}
+$(document).keyup(function (e) {
+  if (e.key === "Escape") {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+  });
+
+  
+  // обработка форм
+  $('.form').each(function(){
+    $(this).validate({
+    errorClass: "invalid",
+    messages: {
+      name: {
+        required: "Please specify your name",
+        minlength: "The name must be at least 2 letters long",
+      },
+      email: {
+        required: "Please specify your emai",
+        email: "Email address format: name@domain.com"
+      },
+      phone: {
+        required: "Please specify your phone number",
+        phone: "Phone number format: +7-(999)-999-99-99",
+        minlength: "Please enter at least 11 characters.",
+        },
+      },
+    });
+  });
+  $(".phone").mask("+7(999) 999-99-99");
+  
   //Слайдер 1
   var categoriesSlider = new Swiper(".slider-categories__container", {
     
@@ -120,4 +186,10 @@ $('.history__button--three').on('click', function onYouTubeIframeAPIReady() {
       'onReady': videoPlay,
     }
   });
- })
+})
+
+function videoPlay(event) {
+   event.target.playVideo();
+ }
+  
+});S
