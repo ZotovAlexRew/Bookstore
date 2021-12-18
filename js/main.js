f605ee9c5430fe50816afb0d469ed14add23d2c0
zotovalexrew <che123zotov@gmail.com>
2021-12-19 01:52:13 +0300

    fix slider

--- js/main.js
+++ js/main.js
@@ -84,7 +84,7 @@ $(document).keyup(function (e) {
     
     breakpoints: {
       320: {slidesPerView: 2, slidesPerColumn: 2},
-      576: {slidesPerView: 2, slidesPerRow: 2},      
+      560: {slidesPerView: 2, slidesPerColumn: 2},      
       767: {slidesPerView: 3, slidesPerColumn: 1, spaceBetween: 0},
       1200: {slidesPerView: 4, slidesPerColumn: 1, spaceBetween: 27}
     },
