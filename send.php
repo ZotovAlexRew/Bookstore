78d31c4c323e736a82840a90b9c7e8849b479747
zotovalexrew <che123zotov@gmail.com>
2021-12-21 15:02:30 +0300

    fix

--- send.php
+++ send.php
@@ -81,7 +81,7 @@ else {$result = "error";}
 }
 
 // Отображение результата
-echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
-//header('Location: thankyou.html');
+//echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+header('Location: thankyou.html');
 
   
